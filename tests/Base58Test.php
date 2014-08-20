d82c59912557c4982461de70d30ea468ba8059eb
Stephen Hill <stephen@gatekiller.co.uk>
2014-08-20 13:49:34 +0100

    Added string tests. Adjusted the structure of this test code.

--- tests/Base58Test.php
+++ tests/Base58Test.php
@@ -4,11 +4,48 @@ use StephenHill\Base58;
 
 class ClassNameTest extends PHPUnit_Framework_TestCase
 {
+	protected $stringTests;
+	protected $intergerTests;
+
+	public function setup()
+	{
+		$this->stringTests = array(
+			'2g' => 'a',
+			'a3gV' => 'bbb',
+			'aPEr' => 'ccc',
+			'tzCkV5Di' => 'hello!',
+			'JxF12TrwUP45BMd' => 'Hello World',
+			'' => '',
+			'2cFupjhnEsSn59qHXstmK2ffpLv2' => 'simply a long string',
+			'1' => "\x00",
+			'jo91waLQA1NNeBmZKUF' => 'this is a test'
+		);
+
+		$this->intergerTests = array(
+			'N' => '21',
+			's' => '50',
+			'2sidE' => '21212121',
+			'16Ho7Hs' => '3471844090'
+		);
+	}
+
+	public function testEncodeInteger()
+	{
+		$base58 = new Base58();
+
+		foreach($this->intergerTests as $encoded => $integer)
+		{
+			$this->assertSame($encoded, $base58->encodeInteger($integer));
+		}
+	}
+
 	public function testEncode()
 	{
 		$base58 = new Base58();
 
-		$helloWorld = 'Hello World';
-		$this->assertSame('JxF12TrwUP45BMd', $base58->encode($helloWorld));
+		foreach ($this->stringTests as $encoded => $string)
+		{
+			$this->assertSame($encoded, $base58->encode($string));
+		}
 	}
 }
