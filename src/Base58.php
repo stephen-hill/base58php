77170d59118424f477686d48ec7b569bff915c55
Stephen Hill <stephen@gatekiller.co.uk>
2014-08-22 16:04:12 +0100

    Improved code so that encoding now works full with all tests passing.

--- src/Base58.php
+++ src/Base58.php
@@ -1,28 +1,123 @@
 <?php
 
+namespace StephenHill;
+
+use InvalidArgumentException;
+
 class Base58
 {
-	static public $alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+	protected $alphabet;
 
-	public static function encode($int) {
-		$base58_string = "";
-		$base = strlen(self::$alphabet);
-		while($int >= $base) {
-			$div = floor($int / $base);
-			$mod = ($int - ($base * $div)); // php's % is broke with >32bit int on 32bit proc
-			$base58_string = self::$alphabet{$mod} . $base58_string;
-			$int = $div;
+	public function __construct($alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
+	{
+		if (is_string($alphabet) === false)
+		{
+			throw new InvalidArgumentException('Argument $alphabet must be a string.');
 		}
-		if($int) $base58_string = self::$alphabet{$int} . $base58_string;
-		return $base58_string;
-	}
-	
-	public static function decode($base58) {
-		$int_val = 0;
-		for($i=strlen($base58)-1,$j=1,$base=strlen(self::$alphabet);$i>=0;$i--,$j*=$base) {
-			$int_val += $j * strpos(self::$alphabet, $base58{$i});
+
+		if (strlen($alphabet) !== 58)
+		{
+			throw new InvalidArgumentException('Argument $alphabet must contain 58 characters.');
 		}
-		return $int_val;
+
+		$this->alphabet = $alphabet;
 	}
+
+	/**
+	 * Convert a string (aka byte array) to an arbitrary-precision decimal
+	 *
+	 * @param string $string String to be converted
+	 * @return string Arbitrary-precision decimal
+	 */
+	public function stringToDecimal($string)
+	{
+		if (is_string($string) === false)
+		{
+			throw new InvalidArgumentException('Argument $string must be of type string.');
+		}
+
+		$characters = unpack('C*', $string);
+		$length = count($characters);
+		$decimal = '0';
+
+		for ($i = 1; $i <= $length; $i++)
+		{
+			$power = bcpow(2, ($length - $i) * 8);
+			$shifted = bcmul($characters[$i], $power);
+			$decimal = bcadd($decimal, $shifted);
+		}
+
+		return (string)$decimal;
+	}
+
+	public function encodeDecimal($decimal)
+	{
+		if (is_string($decimal) === false)
+		{
+			throw new InvalidArgumentException('Argument $decimal must be of type string.');
+		}
+
+		$base = strlen($this->alphabet);
+		$output = '';
+
+		$leadingZeroCount = 0;
+		foreach (str_split($decimal) as $numeral)
+		{
+			if ($numeral === '0')
+			{
+				$leadingZeroCount++;
+				continue;
+			}
+
+			break;
+		}
+
+		while($decimal >= $base)
+		{
+			$div = bcdiv($decimal, $base);
+			$mod = bcmod($decimal, $base);
+			$output .= $this->alphabet[$mod];
+			$decimal = $div;
+		}
+
+		if ($decimal > 0)
+		{
+			$output .= $this->alphabet[$decimal];
+		}
+
+		$output = str_repeat($this->alphabet[0], $leadingZeroCount) . $output;
+
+		return strrev($output);
+	}
+
+	/**
+	 * @param string $string The string you wish to encode.
+	 * @return string The Base58 encoded string.
+	 */
+	public function encode($string)
+	{
+		if (is_string($string) === false)
+		{
+			throw new InvalidArgumentException('Argument $alphabet must be a string.');
+		}
+
+		if (strlen($string) === 0)
+		{
+			return '';
+		}
+
+		$decimal = $this->stringToDecimal($string);
+		$encoded = $this->encodeDecimal($decimal);
+
+		return (string)$encoded;
+	}
+
+	// public function decode($base58) {
+	// 	$int_val = 0;
+	// 	for($i=strlen($base58)-1,$j=1,$base=strlen(self::$alphabet);$i>=0;$i--,$j*=$base) {
+	// 		$int_val += $j * strpos(self::$alphabet, $base58{$i});
+	// 	}
+	// 	return $int_val;
+	// }
 }
 
--- tests/Base58Test.php
+++ tests/Base58Test.php
@@ -2,14 +2,25 @@
 
 use StephenHill\Base58;
 
-class ClassNameTest extends PHPUnit_Framework_TestCase
+class Base58Tests extends PHPUnit_Framework_TestCase
 {
+	protected $binaryStringToDecimalTests;
 	protected $stringTests;
 	protected $intergerTests;
 
 	public function setup()
 	{
+		$this->binaryStringToDecimalTests = array(
+			'a' => '97',
+			'bbb' => '6447714',
+			'Hello World' => '87521618088882533792115812',
+			'simply a long string' => '658885050385564465925592505944209249682185612903',
+			'N' => '78',
+			"\x00" => 0
+		);
+
 		$this->stringTests = array(
+			'r' => '1',
 			'2g' => 'a',
 			'a3gV' => 'bbb',
 			'aPEr' => 'ccc',
@@ -18,34 +29,58 @@ class ClassNameTest extends PHPUnit_Framework_TestCase
 			'' => '',
 			'2cFupjhnEsSn59qHXstmK2ffpLv2' => 'simply a long string',
 			'1' => "\x00",
-			'jo91waLQA1NNeBmZKUF' => 'this is a test'
+			'jo91waLQA1NNeBmZKUF' => 'this is a test',
+			'NK2qR8Vz63NeeAJp9XRifbwahu' => 'the quick brown fox',
+			'GRvKwF9B69ssT67JgRWxPQTZ2X' => 'THE QUICK BROWN FOX'
 		);
 
 		$this->intergerTests = array(
 			'N' => '21',
 			's' => '50',
+			'1' => '0',
 			'2sidE' => '21212121',
-			'16Ho7Hs' => '3471844090'
+			'6Ho7Hs' => '3471844090'
 		);
 	}
 
-	public function testEncodeInteger()
+	public function testStringToDecimal()
 	{
 		$base58 = new Base58();
 
-		foreach($this->intergerTests as $encoded => $integer)
+		foreach($this->binaryStringToDecimalTests as $string => $decimal)
 		{
-			$this->assertSame($encoded, $base58->encodeInteger($integer));
+			$string = (string)$string;
+			$decimal = (string)$decimal;
+			$this->assertSame($decimal, $base58->stringToDecimal($string));
 		}
 	}
 
+	public function testEncodeDecimal()
+	{
+		$base58 = new Base58();
+
+		foreach($this->intergerTests as $encoded => $decimal)
+		{
+			$decimal = (string)$decimal;
+			$encoded = (string)$encoded;
+
+			$this->assertSame($encoded, $base58->encodeDecimal($decimal));
+		}
+	}
+
+	/**
+	 * @depends testStringToDecimal
+	 * @depends testEncodeDecimal
+	 */
 	public function testEncode()
 	{
 		$base58 = new Base58();
 
 		foreach ($this->stringTests as $encoded => $string)
 		{
-			$this->assertSame($encoded, $base58->encode($string));
+			$string = (string)$string;
+			$encoded = (string)$encoded;
+			$this->assertSame($encoded, $base58->encode($string), "Testing {$string} encodes to {$encoded}.");
 		}
 	}
 }
