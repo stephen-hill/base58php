d18c4f6d99e606a3e94f7c12044f0233798263a8
Stephen Hill <stephen@gatekiller.co.uk>
2014-08-22 16:24:07 +0100

    Tidied up this test code.

--- tests/Base58Test.php
+++ tests/Base58Test.php
@@ -4,34 +4,19 @@ use StephenHill\Base58;
 
 class Base58Tests extends PHPUnit_Framework_TestCase
 {
-	protected $binaryStringToDecimalTests;
-	protected $stringTests;
-	protected $intergerTests;
+	protected $stringToDecinalTestArray;
+	protected $decimalToBase58TestArray;
+	protected $stringToBase58TestArray;
 
 	public function setup()
 	{
-		$this->binaryStringToDecimalTests = array(
+		$this->stringToDecinalTestArray = array(
+			"\x00" => 0,
+			'N' => '78',
 			'a' => '97',
 			'bbb' => '6447714',
 			'Hello World' => '87521618088882533792115812',
-			'simply a long string' => '658885050385564465925592505944209249682185612903',
-			'N' => '78',
-			"\x00" => 0
-		);
-
-		$this->stringTests = array(
-			'r' => '1',
-			'2g' => 'a',
-			'a3gV' => 'bbb',
-			'aPEr' => 'ccc',
-			'tzCkV5Di' => 'hello!',
-			'JxF12TrwUP45BMd' => 'Hello World',
-			'' => '',
-			'2cFupjhnEsSn59qHXstmK2ffpLv2' => 'simply a long string',
-			'1' => "\x00",
-			'jo91waLQA1NNeBmZKUF' => 'this is a test',
-			'NK2qR8Vz63NeeAJp9XRifbwahu' => 'the quick brown fox',
-			'GRvKwF9B69ssT67JgRWxPQTZ2X' => 'THE QUICK BROWN FOX'
+			'simply a long string' => '658885050385564465925592505944209249682185612903'
 		);
 
 		$this->intergerTests = array(
@@ -41,17 +26,33 @@ class Base58Tests extends PHPUnit_Framework_TestCase
 			'2sidE' => '21212121',
 			'6Ho7Hs' => '3471844090'
 		);
+
+		$this->stringToBase58TestArray = array(
+			'' => '',
+			'r' => '1',
+			'2g' => 'a',
+			'1' => "\x00",
+			'a3gV' => 'bbb',
+			'aPEr' => 'ccc',
+			'tzCkV5Di' => 'hello!',
+			'JxF12TrwUP45BMd' => 'Hello World',
+			'jo91waLQA1NNeBmZKUF' => 'this is a test',
+			'NK2qR8Vz63NeeAJp9XRifbwahu' => 'the quick brown fox',
+			'GRvKwF9B69ssT67JgRWxPQTZ2X' => 'THE QUICK BROWN FOX',
+			'2cFupjhnEsSn59qHXstmK2ffpLv2' => 'simply a long string'
+		);
 	}
 
 	public function testStringToDecimal()
 	{
 		$base58 = new Base58();
 
-		foreach($this->binaryStringToDecimalTests as $string => $decimal)
+		foreach($this->stringToDecinalTestArray as $string => $decimal)
 		{
 			$string = (string)$string;
 			$decimal = (string)$decimal;
-			$this->assertSame($decimal, $base58->stringToDecimal($string));
+
+			$this->assertSame($decimal, $base58->stringToDecimal($string), "Testing {$string} encodes to {$decimal}.");
 		}
 	}
 
@@ -64,7 +65,7 @@ class Base58Tests extends PHPUnit_Framework_TestCase
 			$decimal = (string)$decimal;
 			$encoded = (string)$encoded;
 
-			$this->assertSame($encoded, $base58->encodeDecimal($decimal));
+			$this->assertSame($encoded, $base58->encodeDecimal($decimal), "Testing {$decimal} encodes to {$encoded}.");
 		}
 	}
 
@@ -76,10 +77,11 @@ class Base58Tests extends PHPUnit_Framework_TestCase
 	{
 		$base58 = new Base58();
 
-		foreach ($this->stringTests as $encoded => $string)
+		foreach ($this->stringToBase58TestArray as $encoded => $string)
 		{
 			$string = (string)$string;
 			$encoded = (string)$encoded;
+
 			$this->assertSame($encoded, $base58->encode($string), "Testing {$string} encodes to {$encoded}.");
 		}
 	}
