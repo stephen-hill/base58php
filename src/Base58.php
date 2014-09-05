78dc1bacb6128bbf0c18280a00ca82d1b1ce6733
Stephen Hill <stephen@gatekiller.co.uk>
2014-09-05 21:26:56 +0100

    Modified to accept an optional service, otherwise a default is selected.

--- src/Base58.php
+++ src/Base58.php
@@ -15,14 +15,9 @@ use InvalidArgumentException;
 class Base58
 {
     /**
-     * @var string
+     * @var StephenHill\ServiceInterface;
      */
-    protected $alphabet;
-
-    /**
-     * @var int
-     */
-    protected $base;
+    protected $service;
 
     /**
      * Constructor
@@ -30,8 +25,15 @@ class Base58
      * @param string $alphabet
      * @since Release v1.0.0
      */
-    public function __construct($alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
-    {
+    public function __construct(
+        $alphabet = null,
+        ServiceInterface $service = null
+    ) {
+        // Handle null alphabet
+        if (is_null($alphabet) === true) {
+            $alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+        }
+
         // Type validation
         if (is_string($alphabet) === false) {
             throw new InvalidArgumentException('Argument $alphabet must be a string.');
@@ -42,8 +44,13 @@ class Base58
             throw new InvalidArgumentException('Argument $alphabet must contain 58 characters.');
         }
 
-        $this->alphabet = $alphabet;
-        $this->base = strlen($alphabet);
+        // Provide a default service if one isn't injected
+        if ($service === null)
+        {
+            $service = new BCMathService($alphabet);
+        }
+
+        $this->service = $service;
     }
 
     /**
@@ -55,57 +62,7 @@ class Base58
      */
     public function encode($string)
     {
-        // Type validation
-        if (is_string($string) === false) {
-            throw new InvalidArgumentException('Argument $string must be a string.');
-        }
-
-        // If the string is empty, then the encoded string is obviously empty
-        if (strlen($string) === 0) {
-            return '';
-        }
-
-        // Strings in PHP are essentially 8-bit byte arrays
-        // so lets convert the string into a PHP array
-        $bytes = array_values(unpack('C*', $string));
-
-        // Now we need to convert the byte array into an arbitrary-precision decimal
-        // We basically do this by performing a base256 to base10 conversion
-        $decimal = $bytes[0];
-
-        for ($i = 1, $l = count($bytes); $i < $l; $i++) {
-            $decimal = bcmul($decimal, 256);
-            $decimal = bcadd($decimal, $bytes[$i]);
-        }
-
-        // This loop now performs base 10 to base 58 conversion
-        // The remainder or modulo on each loop becomes a base 58 character
-        $output = '';
-        while ($decimal >= $this->base) {
-            $div = bcdiv($decimal, $this->base);
-            $mod = bcmod($decimal, $this->base);
-            $output .= $this->alphabet[$mod];
-            $decimal = $div;
-        }
-
-        // If there's still a remainder, append it
-        if ($decimal > 0) {
-            $output .= $this->alphabet[$decimal];
-        }
-
-        // Now we need to reverse the encoded data
-        $output = strrev($output);
-
-        // Now we need to add leading zeros
-        foreach ($bytes as $byte) {
-            if ($byte === 0) {
-                $output = $this->alphabet[0] . $output;
-                continue;
-            }
-            break;
-        }
-
-        return (string) $output;
+        return $this->service->encode($string);
     }
 
     /**
@@ -117,51 +74,6 @@ class Base58
      */
     public function decode($base58)
     {
-        // Type Validation
-        if (is_string($base58) === false) {
-            throw new InvalidArgumentException('Argument $base58 must be a string.');
-        }
-
-        // If the string is empty, then the decoded string is obviously empty
-        if (strlen($base58) === 0) {
-            return '';
-        }
-
-        $indexes = array_flip(str_split($this->alphabet));
-        $chars = str_split($base58);
-
-        // Check for invalid characters in the supplied base58 string
-        foreach ($chars as $char) {
-            if (isset($indexes[$char]) === false) {
-                throw new InvalidArgumentException('Argument $base58 contains invalid characters.');
-            }
-        }
-
-        // Convert from base58 to base10
-        $decimal = $indexes[$chars[0]];
-
-        for ($i = 1, $l = count($chars); $i < $l; $i++) {
-            $decimal = bcmul($decimal, 58);
-            $decimal = bcadd($decimal, $indexes[$chars[$i]]);
-        }
-
-        // Convert from base10 to base256 (8-bit byte array)
-        $output = '';
-        while ($decimal > 0) {
-            $byte = bcmod($decimal, 256);
-            $output = pack('C', $byte) . $output;
-            $decimal = bcdiv($decimal, 256);
-        }
-
-        // Now we need to add leading zeros
-        foreach ($chars as $char) {
-            if ($indexes[$char] === 0) {
-                $output = "\x00" . $output;
-                continue;
-            }
-            break;
-        }
-
-        return $output;
+        return $this->service->decode($base58);
     }
 }
