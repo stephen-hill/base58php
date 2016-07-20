dc42e86872bc2a5a83145c17ab080d0a2d3e426d
Stephen Hill <stephen@gatekiller.co.uk>
2016-07-20 21:01:04 +0100

    Setting "bcmath.scale" to something other than 0 so that we can be sure bcdiv's $scale parameter is working correctly.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,7 @@
 
 require_once 'vendor/autoload.php';
 date_default_timezone_set("UTC");
+
+// Setting "bcmath.scale" to something other than 0 so that
+// we can be sure bcdiv's $scale parameter is working correctly
+bcscale(16);
